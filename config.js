b3ebfb620a24581b23bf5a7bfa2355bf38282192
Pasiura Daryna <107425596+Anirad2002@users.noreply.github.com>
2024-05-13 20:49:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,5 @@
-// config.js
-// Конфігураційні параметри для вашого додатку
-
 module.exports = {
-    // Параметри підключення до бази даних MongoDB
     mongoURI: 'mongodb://localhost/MVC_project_przepis',
-    // Порт, на якому запускається сервер
     port: 3000,
-    // Секретний ключ для підпису токенів автентифікації
     jwtSecret: 'supersecretjwtkey'
 };
